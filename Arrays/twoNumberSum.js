e2a439d8e3a7411d0e6ea0e2cab404e8dbceed60
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-01-20 16:42:44 -0500

    third solution to twoNumberSum

--- Arrays/twoNumberSum.js
+++ Arrays/twoNumberSum.js
@@ -17,7 +17,7 @@ function twoNumberSum1(array, targetSum) {
   return retArr;
 }
 // Time: O(N^2) for iterating N times through an array of N values
-// Space:  O(1) for a dynamic array, O(N) for a static array
+// Space:  O(N) to create an array
 console.log("solution 1: ", twoNumberSum1([3, 5, -4, 8, 11, 1, -1, 6], 10));
 
 // Solution 2: Look for Y in X + Y = targetSum
@@ -38,5 +38,27 @@ function twoNumberSum2(array, targetSum) {
   return retArr;
 }
 // Time:  O(N^2) still iterating N times through array of N values
-// Space: O(1)
+// Space: O(N)
 console.log("solution 2: ", twoNumberSum2([3, 5, -4, 8, 11, 1, -1, 6], 10));
+
+// Solution 3:  Hash Map
+function twoNumberSum3(array, targetSum) {
+  // Write your code here.
+  let retArr = [];
+  let nums = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    let num1 = array[i];
+    let num2 = targetSum - num1;
+
+    if (nums.has(num2)) {
+      retArr.push(num1, num2);
+      break;
+    }
+    nums.add(num1);
+  }
+  return retArr;
+}
+// Time: O(N) iterate up to N times through the array
+// Space: O(N) to store the hash map and the return array
+console.log("solution 3: ", twoNumberSum3([3, 5, -4, 8, 11, 1, -1, 6], 10));
