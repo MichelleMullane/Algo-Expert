9b5fbb67492cb9b16b68c74c289b30bb4a969621
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-10-24 10:37:00 -0400

    added another solution for group anagrams

--- Arrays/groupAnagrams.js
+++ Arrays/groupAnagrams.js
@@ -37,7 +37,7 @@ var groupAnagrams1 = function (strs) {
   // return the result
   return Object.values(result);
 };
-// Time complexity of this solution is O(m*nlog(n)) to loop through the strs array of n values and sort a string of m values.
+// Time complexity of this solution is O(m*nlog(n)) to loop through the strs array of m values and sort a string of n values.
 console.log("Expected result (can be in any order): ", [
   ["bat"],
   ["nat", "tan"],
@@ -46,4 +46,31 @@ console.log("Expected result (can be in any order): ", [
 console.log(
   "Solution 1 result: ",
   groupAnagrams1(["eat", "tea", "tan", "ate", "nat", "bat"])
+);
+
+// Solution 2:  Implement a hash map with a "map" of utf-8 encoding for each character in the string as the key
+var groupAnagrams2 = function (strs) {
+  let result = {};
+
+  for (let str of strs) {
+    let count = new Array(26).fill(0);
+
+    for (let char of str) {
+      count[char.charCodeAt() - "a".charCodeAt(0)]++;
+    }
+
+    let key = count.join("#");
+
+    if (result[key]) {
+      result[key].push(str);
+    } else {
+      result[key] = [str];
+    }
+  }
+  return Object.values(result);
+};
+// Time complexity is slightly better without sorting.  O(n*m) for iterating through the strs array of n characters.
+console.log(
+  "Solution 2 result: ",
+  groupAnagrams2(["eat", "tea", "tan", "ate", "nat", "bat"])
 );
