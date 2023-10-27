daedb447a499a7b1b2bc8f8f3b68a8d51cccd0ee
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2023-10-27 09:22:34 -0400

    completed another solution for Remove Element

--- Arrays/removeElement.js
+++ Arrays/removeElement.js
@@ -6,7 +6,7 @@
 // iterate k counter
 // return k
 
-var removeElement = function (nums, val) {
+var removeElement1 = function (nums, val) {
   let k = 0;
 
   for (let i = 0; i < nums.length; i++) {
@@ -17,6 +17,34 @@ var removeElement = function (nums, val) {
   }
   return k;
 };
-
+// This solution has O(n) time complexity to iterate through an array of n values.  This beats ~62% of leetcode users for runtime.
+// This has O(1) memory as it changes the array in place, and I allocate counter variables.  This beats ~80% of leetcode users.
 console.log("Expected result: ", 5);
-console.log("Output: ", removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+console.log("Output: ", removeElement1([0, 1, 2, 2, 3, 0, 4, 2], 2));
+
+
+// Solution 2:
+var removeElement2 = function (nums, val) {
+  let left = 0;
+  let right = 0;
+
+  while (right < nums.length) {
+    if (nums[right] === val) {
+      right++;
+      continue;
+    }
+
+    if (nums[left] === val) {
+      nums[left] = nums[right];
+      nums[right] = val;
+    }
+
+    right++;
+    left++;
+  }
+  return left;
+};
+// I revisited this problem after a few months.  Solution 2 has O(n) time complexity to iterate through an array of n values.  This beats ~98% of leetcode users, so it is slightly more time-efficient than Solution 1.
+// This solution is O(1) constant time for memory to allocate 2 pointer variables.  This beats ~40% of leetcode users for memory, so not as memory-efficient as Solution 1.
+console.log("Expected result: ", 5);
+console.log("Output: ", removeElement2([0, 1, 2, 2, 3, 0, 4, 2], 2));
