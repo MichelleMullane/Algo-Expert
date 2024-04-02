e199b5ab4cc44f34962388844398aad064191ac1
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2024-04-02 09:12:51 -0400

    added another solution to compress

--- Strings/compress.js
+++ Strings/compress.js
@@ -28,8 +28,37 @@ const compress = (s) => {
   return response;
 };
 
-//Time: O(N) to iterate through the string of length N characters
+//Time: O(N^2) to iterate through the string of length N characters and concatenate the immutable response array
 //Space: O(N) to create the response array based on the input array of N characters
 
 console.log("Expected result: 2c3at3s");
 console.log("Test result: ", compress("ccaaatsss"));
+
+// The solution below is linear run time by pushing to an array rather than concatenating a string.
+const compress2 = (s) => {
+  let response = [];
+  let left = 0;
+  let right = 1;
+
+  while (right <= s.length) {
+    if (s[right] === s[left]) {
+      right++;
+    } else {
+      if (right - left === 1) {
+        response.push(s[left]);
+      } else {
+        response.push(parseInt(right - left), s[left]);
+      }
+
+      left = right;
+      right++;
+    }
+  }
+
+  return response.join("");
+};
+//Time: O(N) to iterate through the string of length N characters
+//Space: O(N) to create the response array based on the input array of N characters
+
+console.log("Expected result: 2c3at3s");
+console.log("Test result: ", compress2("ccaaatsss"));
