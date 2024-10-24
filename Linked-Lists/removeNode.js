12982a4394c5e940ed8d238d8c34a44fb10965ba
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2024-10-24 10:57:44 -0400

    added removeNode problem to linked lists

---
+++ Linked-Lists/removeNode.js
@@ -0,0 +1,56 @@
+// Initiate a linked list for testing
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// Solution 1: Iterative
+const removeNode = (head, targetVal) => {
+  if (head.val === targetVal) {
+    return head.next;
+  }
+
+  let current = head;
+  let previous = null;
+
+  while (current != null) {
+    if (current.val === targetVal) {
+      previous.next = current.next;
+      break;
+    }
+
+    previous = current;
+    current = current.next;
+  }
+
+  return head;
+};
+// Time complexity: O(n) to iterate through n nodes of the linked list
+// Space complexity: O(1) to save a constant number of variables
+console.log("Expected result: a -> b -> d");
+console.log("Test result solution 1: ", removeNode(a, "c"));
+
+// Solution 2: Recursive
+const removeNodeRecursive = (head, targetVal) => {
+  if (head === null) return null;
+
+  if (head.val === targetVal) return head.next;
+
+  head.next = removeNodeRecursive(head.next, targetVal);
+
+  return head;
+};
+// Same time and space complexity as recursive solution above, but a bit simpler without passing additional variables.
+console.log("Expected result: a -> b -> d");
+console.log("Test result solution 3: ", removeNodeRecursive(a, "c"));
