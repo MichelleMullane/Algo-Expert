b7e81453542bb8b04e1f7c69384efcc9b694ed44
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2024-11-18 11:12:16 -0500

    added recursive solution for tree sum algo

--- Binary-Trees/treeSum.js
+++ Binary-Trees/treeSum.js
@@ -9,7 +9,7 @@ class Node {
 const a = new Node(3);
 const b = new Node(11);
 const c = new Node(4);
-const d = new Node(4);
+const d = new Node(5);
 const e = new Node(-2);
 const f = new Node(1);
 
@@ -38,5 +38,19 @@ const treeSum = (root) => {
 };
 // Time Complexity: O(N) to traverse a tree of N nodes
 // Space Complexity: O(N) to create the stack of N nodes
-console.log("Expected result: ", 21);
+console.log("Expected result: ", 22);
 console.log("Test result: ", treeSum(a));
+
+// Solution 2: Recursive
+const treeSum2 = (root) => {
+  if (root === null) return 0;
+
+  const leftTotal = treeSum2(root.left);
+  const rightTotal = treeSum2(root.right);
+
+  return root.val + leftTotal + rightTotal;
+};
+// Time Complexity: O(N) to call the function recursively N times based on the number of nodes in the input tree
+// Space Complexity: O(N) to add N calls to the call stack
+console.log("Expected result: ", 22);
+console.log("Test result: ", treeSum2(a));
